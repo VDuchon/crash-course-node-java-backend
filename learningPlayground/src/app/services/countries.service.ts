a74add444c87644769757f1517ae879c4e30d8c1
Vojtech Duchoň (Ext) <vojtech.duchon@gmail.com>
2023-07-13 12:13:36 +0200

    Make countries project general for all learning

--- learningPlayground/src/app/services/countries.service.ts
+++ learningPlayground/src/app/services/countries.service.ts
@@ -4,12 +4,12 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-
 export class CountriesService {
 
   constructor(private _httpClient: HttpClient) { }
 
-  public getCountries(): any {
-    return this._httpClient.get("https://restcountries.com/v3.1/all");
+  public getCountries():any {
+    return this._httpClient.get("https://restcountries.com/v3.1/all")
   }
+
 }
